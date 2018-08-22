0e1108914c1faf30b5757d3a015541e98028fb21
haiktvn1 <nhonnh9939@gmail.com>
2018-08-23 02:29:35 +0700

    update comment

--- src/config.js
+++ src/config.js
@@ -1,14 +1,14 @@
 var abiTemp = require("./ABI.json")
 var config = {
   uportAppName: "Filemanager",
-  contract: {
+  contract: { // thay address và ABI của smart contract tại đây
     address: "0x82e39D6EC81DED52771669ab942E9984c914f7be",
-    abi: abiTemp,
+    abi: abiTemp, // thay ABI của smart contract
   },
-  uport: {
+  uport: { // thay bằng các thông số mà uport cung cấp tại đây
     mnidAddress: "2ovMuFxV6zykSG5CAvTGPGsBsMVPid8ysFN",
     signingKey:
-      "4dbe967bedbed5ac0ee76e9d41717b3760b62e677e12f5d421c88ae31c17dee4",
+      "4dbe967bedbed5ac0ee76e9d41717b3760b62e677e12f5d421c88ae31c17dee4", 
     appName: "Filemanager"
   }
 };
